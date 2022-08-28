e2fd448cd66d9546e6350a4a4bd56a386bcbb20d
trevc <trevc@LAPTOP-8QT045TA.unl.edu>
2022-08-28 17:04:53 -0500

    Partner added name and major

--- src/Hello.java
+++ src/Hello.java
@@ -11,5 +11,7 @@ public class Hello {
 	public static void main(String args[]) {
 	 System.out.println("Ben Fritz");
 	 System.out.println("Software Engineer");
+	 System.out.println("Trevor Hoffman");
+	 System.out.println("Computer Science");
 	}
 }
