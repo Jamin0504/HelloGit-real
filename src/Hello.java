7d2eb38e39986450c699692b426e036290426a72
bfrit <bfrit@LAPTOP-AO1PR0VA.unl.edu>
2022-08-28 16:35:52 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Ben Fritz
+ * 
+ * Date 2022/28/08
+ * 
+ * Basic "Hello World" Project in Java
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+	 System.out.println("Ben Fritz");
+	}
+}
